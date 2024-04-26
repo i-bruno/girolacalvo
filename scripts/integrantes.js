05beabf39c39008d38b49b2b218ce5c048943c27
Ignacio Bruno <ignaciobruno90@gmail.com>
2024-04-26 16:33:47 -0300

    Se prueba el slide swiper con datos del json

--- scripts/integrantes.js
+++ scripts/integrantes.js
@@ -1,15 +1,16 @@
 
-// Fetch del archivo JSON
-fetch('./json/integrantesGirola.json')
-    .then(response => response.json())
-    .then(data => {
-        const swiperWrapper = document.getElementById('swiper-wrapper');
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch del archivo JSON
+    fetch('./json/integrantesGirola.json')
+        .then(response => response.json())
+        .then(data => {
+            const swiperWrapper = document.getElementById('swiper-wrapper');
 
-        // Iterar sobre los datos del JSON y crear swiper-slide para cada integrante
-        data.forEach(integrante => {
-            const slide = document.createElement('div');
-            slide.classList.add('swiper-slide');
-            slide.innerHTML = `
+            // Iterar sobre los datos del JSON y crear swiper-slide para cada integrante
+            data.forEach(integrante => {
+                const slide = document.createElement('div');
+                slide.classList.add('swiper-slide');
+                slide.innerHTML = `
                 <h5>Nombre:</h5>
                 <p>${integrante.nombre}</p>
                 <h5>Cumpleaños:</h5>
@@ -17,16 +18,17 @@ fetch('./json/integrantesGirola.json')
                 <h5>Cuerda:</h5>
                 <p>${integrante.cuerda}</p>
             `;
-            swiperWrapper.appendChild(slide);
-        });
+                swiperWrapper.appendChild(slide);
+            });
 
-        // Inicializar Swiper después de haber agregado todos los swiper-slide
-        const swiper = new Swiper('.swiper-container', {
-            loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-        });
-    })
-    .catch(error => console.error('Error al cargar los datos:', error));
+            // Inicializar Swiper después de haber agregado todos los swiper-slide
+            const swiper = new Swiper('.swiper-container', {
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+            });
+        })
+        .catch(error => console.error('Error al cargar los datos:', error));
+});
