bd2ea4d69ec1ae37689fc3b8597216163803b0a9
Ignacio Bruno <ignaciobruno90@gmail.com>
2024-04-26 13:01:42 -0300

    prueba script

--- scripts/fechas.js
+++ scripts/fechas.js
@@ -1,79 +1,32 @@
-// function calcularDiferenciaEnDias(fechaFutura) {
-//     // Obtenemos la fecha actual
-//     let fechaActual = new Date();
+function calcularDiferenciaEnDias(fechaFutura) {
+  // Obtenemos la fecha actual
+  let fechaActual = new Date();
 
-//     // Convertimos la fecha futura a objeto Date si no lo es
-//     if (!(fechaFutura instanceof Date)) {
-//         fechaFutura = new Date(fechaFutura);
-//     }
+  // Convertimos la fecha futura a objeto Date si no lo es
+  if (!(fechaFutura instanceof Date)) {
+    fechaFutura = new Date(fechaFutura);
+  }
 
-//     // Calculamos la diferencia en milisegundos
-//     let diferencia = fechaFutura.getTime() - fechaActual.getTime();
+  // Calculamos la diferencia en milisegundos
+  let diferencia = fechaFutura.getTime() - fechaActual.getTime();
 
-//     // Convertimos la diferencia de milisegundos a días
-//     let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  // Convertimos la diferencia de milisegundos a días
+  let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
 
-//     return dias;
-// }
-
-
-// Función para calcular la diferencia en días y establecer el contenido y color del badge
-function establecerBadge(fechaFutura, idBadge) {
-    // Obtenemos la fecha actual
-    let fechaActual = new Date();
-
-    // Convertimos la fecha futura a objeto Date si no lo es
-    if (!(fechaFutura instanceof Date)) {
-        fechaFutura = new Date(fechaFutura);
-    }
-
-    // Ajustamos las fechas al huso horario local
-    fechaActual.setHours(0, 0, 0, 0);
-    fechaFutura.setHours(0, 0, 0, 0);
-
-    // Calculamos la diferencia en milisegundos
-    let diferencia = fechaFutura.getTime() - fechaActual.getTime();
-
-    // Convertimos la diferencia de milisegundos a días
-    let dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-    console.log(dias);
-
-    const badge = document.getElementById(idBadge);
-
-    if (dias > 0) {
-        badge.innerText = dias + " días";
-    } else if (dias === 0) {
-        badge.innerText = "Hoy";
-    } else {
-        badge.innerText = "Pasado";
-    }
-
-    // Aplicar estilos al badge según la situación
-    if (dias === 0) {
-        badge.classList.remove("bg-secondary");
-        badge.classList.add("bg-danger");
-    } else if (dias < 0) {
-        badge.classList.remove("bg-secondary");
-        badge.classList.add("bg-success");
-    } else {
-        badge.classList.remove("bg-danger", "bg-success");
-        badge.classList.add("bg-secondary");
-    }
+  return dias;
 }
 
 // Función para formatear la fecha en formato argentino (dd/mm/yyyy)
 function formatoFechaArgentina(fecha) {
-    const fechaFormateada = new Date(fecha);
-    return fechaFormateada.toLocaleDateString("es-AR");
+  const fechaFormateada = new Date(fecha);
+  return fechaFormateada.toLocaleDateString("es-AR");
 }
 
-
 // // Definimos las fechas futuras
-// let pujoleadaFecha = new Date('2024-05-25');
-// let festitapJunioFecha = new Date('2024-06-28');
-// let ecunhiFecha = new Date('2024-10-05');
-// let festitapDiciembreFecha = new Date('2024-12-13');
-
+let pujoleadaFecha = new Date("2024-05-25");
+let festitapJunioFecha = new Date("2024-06-28");
+let ecunhiFecha = new Date("2024-10-05");
+let festitapDiciembreFecha = new Date("2024-12-13");
 
 // // Calculamos los días restantes y los mostramos en los badges
 // document.getElementById('pujoleada-badge').innerText = calcularDiferenciaEnDias(pujoleadaFecha) + ' días';
